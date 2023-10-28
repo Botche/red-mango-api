c733c9faf2ee1ef036145ea5fd62536c38719687
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-10-28 09:16:25 +0300

    Get Individual Menu Item

--- src/RedMangoAPI/Controllers/MenuItemController.cs
+++ src/RedMangoAPI/Controllers/MenuItemController.cs
@@ -3,6 +3,7 @@
     using System.Net;
 
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
 
     using RedMangoAPI.Data;
     using RedMangoAPI.Models;
@@ -22,9 +23,31 @@
         }
 
         [HttpGet]
-        public IActionResult GetMenuItems()
+        public async Task<IActionResult> GetMenuItems()
         {
-            response.Result = this.dbContext.MenuItems.ToList();
+            response.Result = await this.dbContext.MenuItems.ToListAsync();
+            response.StatusCode = HttpStatusCode.OK;
+
+            return this.Ok(response);
+        }
+
+        [HttpGet("{id:Guid}")]
+        public async Task<IActionResult> GetMenuItem(Guid id)
+        {
+            if (id == Guid.Empty) 
+            {
+                response.StatusCode = HttpStatusCode.BadRequest;
+                return this.BadRequest(response);
+            }
+
+            var menuItem = await this.dbContext.MenuItems.FindAsync(id);
+            if (menuItem == null)
+            {
+                response.StatusCode = HttpStatusCode.NotFound;
+                return this.NotFound(response);
+            }
+
+            response.Result = menuItem;
             response.StatusCode = HttpStatusCode.OK;
 
             return this.Ok(response);
