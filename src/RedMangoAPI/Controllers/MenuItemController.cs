7ee25874e1dbf9050a54a434140c5d1b04a00b3b
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-10-28 09:09:18 +0300

    Get Menu Item and API Response

---
+++ src/RedMangoAPI/Controllers/MenuItemController.cs
@@ -0,0 +1,33 @@
+﻿namespace RedMangoAPI.Controllers
+{
+    using System.Net;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using RedMangoAPI.Data;
+    using RedMangoAPI.Models;
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        private readonly RedMangoDbContext dbContext;
+        private readonly ApiResponse response;
+
+        public MenuItemController(RedMangoDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            this.response = new ApiResponse();
+        }
+
+        [HttpGet]
+        public IActionResult GetMenuItems()
+        {
+            response.Result = this.dbContext.MenuItems.ToList();
+            response.StatusCode = HttpStatusCode.OK;
+
+            return this.Ok(response);
+        }
+    }
+}
---
+++ src/RedMangoAPI/Models/ApiResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace RedMangoAPI.Models
+{
+    using System.Net;
+
+    public class ApiResponse
+    {
+        public ApiResponse()
+        {
+            this.ErrorMessages = new List<string>();
+            this.IsSuccess = true;
+        }
+
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; }
+        public List<string> ErrorMessages { get; set; }
+        public object Result { get; set; }
+    }
+}
