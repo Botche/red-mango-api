bf70c2e389e7e7b4d7c6ccdfd814f0b8dc5756c4
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-10-29 09:15:31 +0200

    Auth Controller and Dependency Injection

---
+++ src/RedMangoAPI/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿namespace RedMangoAPI.Controllers
+{
+    using AutoMapper;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    using RedMangoAPI.Database;
+    using RedMangoAPI.Database.Entities;
+    using RedMangoAPI.Models.Dto.Identity;
+
+    public class AuthController : BaseApiController
+    {
+        private readonly string secretKey;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AuthController(RedMangoDbContext dbContext, 
+            IMapper mapper, 
+            IConfiguration configuration, 
+            UserManager<ApplicationUser> userManager, 
+            RoleManager<IdentityRole> roleManager
+        ) : base(dbContext, mapper)
+        {
+            this.secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+    }
+}
