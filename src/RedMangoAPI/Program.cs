f9b4ba30ef0cb5fb3c78bc5151a09b8bcefb13c6
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-10-29 10:30:29 +0200

    Add security to Swagger

--- src/RedMangoAPI/Program.cs
+++ src/RedMangoAPI/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 using RedMangoAPI.Database;
 using RedMangoAPI.Database.Entities;
@@ -69,6 +70,34 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
     options.OperationFilter<IgnorePropertyFilter>();
+
+    options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme()
+    {
+        Description =
+            "JWT Authorization header using the Bearer scheme.\r\n\r\n" +
+            "Enter: 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
+            "Example: \"Bearer 12345abcdef\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = JwtBearerDefaults.AuthenticationScheme,
+                },
+                Scheme = "oauth2",
+                Name = JwtBearerDefaults.AuthenticationScheme,
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
 });
 
 List<Assembly> assemblies = new List<Assembly>()
