7cd3e25d89138f478902f32f407c5769150a4057
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-10-30 21:02:36 +0200

    Add Shopping Cart and Cart Item models

---
+++ src/RedMangoAPI.Database/RedMangoAPI.Database.Entities/CartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace RedMangoAPI.Database.Entities
+{
+    public class CartItem : BaseEntity
+    {
+        public Guid MenuItemId { get; set; }
+        public virtual MenuItem MenuItem { get; set; }
+        public int Quantity { get; set; }
+        public Guid ShoppingCartId { get; set; }
+        public virtual ShoppingCart ShoppingCart { get; set; }
+    }
+}
---
+++ src/RedMangoAPI.Database/RedMangoAPI.Database.Entities/ShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿namespace RedMangoAPI.Database.Entities
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class ShoppingCart : BaseEntity
+    {
+        public ShoppingCart()
+        {
+            this.CartItems = new HashSet<CartItem>();
+        }
+
+        public Guid UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        public string StripePaymentIntentId { get; set; }
+        public string ClientSecret { get; set; }
+
+        public ICollection<CartItem> CartItems { get; set; }
+
+        [NotMapped]
+        public double CartTotal { get; set; }
+    }
+}
